0024471d03eaee2902b65cab882730a4810c5426
szyba0102 <wszybinska@student.agh.edu.pl>
2022-02-01 15:19:14 +0100

    delate

--- jumperProject/src/main/java/agh/ics/oop/Main.java
+++
@@ -1,9 +0,0 @@
-package agh.ics.oop;
-
-import agh.ics.oop.gui.App;
-import javafx.application.Application;
-
-
-public class Main {
-    public static void main(String[] args) {Application.launch(App.class, args);}
-}
